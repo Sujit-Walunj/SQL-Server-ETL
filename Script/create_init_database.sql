ee4dbb8bd5f50859bfb3bcb274683517c3e2cbe7
Sujit-Walunj <161869070+Sujit-Walunj@users.noreply.github.com>
2026-09-01 08:42:34 +0530

    Create init database script for Datawarehouse
    Drop existing DATAWAREHOUSE database if it exists and create a new one with bronze, silver, and gold schemas.

--- Script/create_init_database.sql
+++ Script/create_init_database.sql
@@ -1,1 +1,23 @@
 
+IF EXISTS ( SELECT 1 FROM sys.databases WHERE name = 'DATAWAREHOUSE')
+	BEGIN
+	ALTER DATABASE DATAWAREHOUSE SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DATAWAREHOUSE;
+END;
+	GO
+
+------- Create Datawarehouse--
+CREATE DATABASE Datawarehouse
+-- slelecting current database
+GO
+USE Datawarehouse;
+GO
+-- Create schema--
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+
+
+
